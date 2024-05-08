016f0d07fe32956f3601ad1df9e721ed503c2395
Annesya Banerjee <annesyab@ranvier.ib.cluster>
2024-05-07 20:17:18 -0400

    separated the speech speaker and joint encoders

--- encoder_saganet.py
+++ encoder_saganet.py
@@ -7,54 +7,44 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import Compose
 from transformers import WhisperForConditionalGeneration, WhisperProcessor, AutoFeatureExtractor, WhisperModel
 
-class Speech_Encoder_Transformer(torch.nn.Module):
-    def __init__(self, sampling_rate=16000,d_model=704,num_head=1,dim_feedforward=512,num_layers=6):
-        super(Speech_Encoder_Transformer, self).__init__()
+class Speech_Encoder(torch.nn.Module):
+    def __init__(self, sampling_rate=16000):
+        super(Speech_Encoder, self).__init__()
         self.sampling_rate = sampling_rate
         self.whisper_encoder = WhisperModel.from_pretrained("openai/whisper-base")
         self.decoder_input_ids = torch.tensor([[1, 1]]) * self.whisper_encoder.config.decoder_start_token_id
-        self.whisper_feature_extractor = AutoFeatureExtractor.from_pretrained("openai/whisper-base")
-        self.ecapa_encoder = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
-
-        # Define downsampling layers
-        # No downsampling needed as output of ECAPA-TDNN pooled and concatenating with the word embedding 
-
-
-        # Define Transformer layers
-        self.transformer_encoder_single = nn.TransformerEncoderLayer(d_model=d_model, nhead=num_head, dim_feedforward=dim_feedforward, batch_first=True)
-    
-        self.transformer_encoder_stack = nn.TransformerEncoder(self.transformer_encoder_single, num_layers=num_layers)
-
-        # Define prediction heads
-        self.next_word_prediction_head = nn.Linear(128,self.word_vocab)
-        self.speaker_recognition_head = nn.Linear(128,self.num_speaker_class)
-        self.speaker_softmax = nn.Softmax(dim=-1)
+        self.whisper_feature_extractor = AutoFeatureExtractor.from_pretrained("openai/whisper-base") 
 
     def forward(self, x):
         # Forward pass for whisper branch
-        x_whisper = self.whisper_feature_extractor(x, sampling_rate=self.sampling_rate, return_tensors="pt").input_features
-        whisper_embedding = self.whisper_encoder(x_whisper, decoder_input_ids=self.decoder_input_ids).encoder_last_hidden_state # shape batch x timeframes x embeddings
+        x = self.whisper_feature_extractor(x, sampling_rate=self.sampling_rate, return_tensors="pt").input_features
+        speech_embedding = self.whisper_encoder(x, decoder_input_ids=self.decoder_input_ids).encoder_last_hidden_state # shape batch x timeframes x embeddings
 
+        return speech_embedding
+    
+
+class Speaker_Encoder(torch.nn.Module):
+    def __init__(self):
+        super(Speaker_Encoder, self).__init__()
+        self.ecapa_encoder = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
+
+    def forward(self, x):
         # Forward pass for ECAPA branch
-        ecapa_embedding = self.ecapa_encoder.encode_batch(x) # shape batch x 1 x embeddings
-        
-        # Concatenate downscaled embeddings
-        # concatenate the word embeddings with speaker embeddings without changing the frame rate 
-        batch, feature, embeddings = whisper_embedding.shape
-        ecapa_embedding = ecapa_embedding.expand(-1, feature, -1)
-        concatenated_embeddings = torch.cat((whisper_embedding, ecapa_embedding), dim=-1)
+        speaker_embedding = self.ecapa_encoder.encode_batch(x) # shape batch x 1 x embeddings
 
-        # Transformer layers
-        transformer_output = self.transformer_encoder_stack(concatenated_embeddings)
+        return speaker_embedding
+    
+class Joint_Encoder(torch.nn.Module):
+    def __init__(self,d_model=704,num_head=1,dim_feedforward=512,num_layers=6):
+        super(Speaker_Encoder, self).__init__()
+        self.transformer_encoder_single = nn.TransformerEncoderLayer(d_model=d_model, nhead=num_head, dim_feedforward=dim_feedforward, batch_first=True)
+        self.joint_encoder = nn.TransformerEncoder(self.transformer_encoder_single, num_layers=num_layers)
 
-        # Task-specific heads
-        # word_prediction = self.next_word_prediction_head(transformer_decoder_output)
-        # speaker_recognition = self.speaker_recognition_head(transformer_output)
-        # speaker_recognition = self.speaker_softmax(speaker_recognition)
+    def forward(self, x):
+        # Forward pass for joint branch
+        joint_embedding = self.joint_encoder(x) # shape batch x timeframes x embeddings
 
-        #next_word_prediction = self.next_word_prediction_head(transformer_output
-
-        return transformer_output
+        return joint_embedding
 
 
 if __name__ == '__main__':
