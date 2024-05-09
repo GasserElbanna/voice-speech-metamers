2e096ce715ce660b9dba5fe7690e16d54f755d7a
GasserElbanna <gelbanna@mit.edu>
2024-05-08 22:37:22 -0400

    :art: update train and learner scripts

--- learner.py
+++ learner.py
@@ -65,39 +65,39 @@ class Learner(pl.LightningModule):
         #pass the concatenated embedding through joint encoder
         features_joint = self.encoder_joint(features_concatenated)
 
+        #get the length of the features and labels
+        features_len = torch.IntTensor([feat.shape[0] for feat in features_joint])
+        labels_speech_len = torch.IntTensor([len(label) for label in batch['speech_labels']])
+        print(features_len, labels_speech_len)
+
+        #pad both features and labels
+        features_joint = pad_sequence(features_joint, batch_first=True)
+        speech_labels = pad_sequence(
+                            batch['speech_labels'],
+                            batch_first=True,
+                            padding_value=self.tokenizer.pad_idx,
+                            )
+
         #get the word and speaker logits
         logits_speech = self.decoder_speech(features_joint)
         logits_speaker = self.decoder_speaker(features_joint)
-
-        #get the length of the features and labels
-        #need to modify this based on our task -- TODO
-        features_len = torch.IntTensor([feat.shape[0] for feat in features_joint])
-        labels_speech_len = torch.IntTensor([len(label) for label in batch['labels_speech']])
-        labels_speaker_len = torch.IntTensor([len(label) for label in batch['labels_speaker']])
-        print(features_len, labels_speech_len, labels_speaker_len)
-
-        #pad both features and labels
-        ## TODO 
-        # features = pad_sequence(features, batch_first=True)
-        # labels = pad_sequence(
-        #                     batch['labels'],
-        #                     batch_first=True,
-        #                     padding_value=self.tokenizer.pad_idx,
-        #                     )
+        
         
         #compute the prob for each class
         log_probs_speech = nn.functional.log_softmax(logits_speech, dim=-1)
-        log_probs_speaker = nn.functional.log_softmax(logits_speaker, dim=-1)
+        # log_probs_speaker = nn.functional.log_softmax(logits_speaker, dim=-1)
 
         #compute CTC loss
         loss_speech = self.ctc_objective(
             log_probs_speech.transpose(0, 1),  # (N, T, C) -> (T, N, C)
-            labels,
+            speech_labels,
             features_len,
-            labels_len,
+            labels_speech_len,
         )
-        loss_speaker = self.cross_entropy_objective(log_probs_speaker,batch['labels_speaker']) ## TODO make sure this is correct
-        loss = loss_speech+loss_speaker
+        loss_speaker = self.cross_entropy_objective(logits_speaker, batch['speaker_labels']) ## TODO make sure this is correct
+        loss = loss_speech + loss_speaker
+
+        acc = (batch['speaker_labels'] == torch.argmax(logits_speaker, dim=1)).float().mean()
 
         pred_tokens = log_probs_speech.argmax(dim=-1)
         filtered_tokens = []
@@ -113,25 +113,24 @@ class Learner(pl.LightningModule):
             self.tokenizer.decode(h) for h in filtered_tokens
         ]
         
-        groundtruth = [self.tokenizer.decode(g.tolist()) for g in batch['labels']]
-        # print('True:', groundtruth)
-        # print('Pred:', hypothesis)
-        # Include accuracy metric for speaker recognition TODO
+        groundtruth = [self.tokenizer.decode(g.tolist()) for g in batch['speech_labels']]
+        
         if step == 'test':
             per_values = [self.metric([hyp], [gt]) for hyp, gt in zip(hypothesis, groundtruth)]
             text = batch['text']
             return loss, per_values, hypothesis, groundtruth, text
 
-        per_value = self.metric(hypothesis, groundtruth)
-        return loss, per_value
+        cer_value = self.metric(hypothesis, groundtruth)
+        return loss, cer_value*100, acc*100
 
     def training_step(self, batch):
         
-        loss, per_value = self._shared_eval_step(batch)
+        loss, cer_value, acc = self._shared_eval_step(batch)
         
         self.log_dict({
                     "Total loss": loss,
-                    "PER": per_value,
+                    "CER": cer_value,
+                    "Accuracy": acc,
                 }, on_step=True, on_epoch=False, sync_dist=True)
         return loss
     
@@ -143,15 +142,17 @@ class Learner(pl.LightningModule):
     def on_validation_epoch_start(self):
          self.val_logs = {
             "val_CTC_loss": 0,
-            "val_PER": 0,
+            "val_CER": 0,
+            "val_Accuracy": 0,
         }
 
     def validation_step(self, batch, _):
         
-        loss, per_value = self._shared_eval_step(batch)
+        loss, cer_value, acc = self._shared_eval_step(batch)
 
         self.val_logs["val_Total_loss"] = loss
-        self.val_logs["val_PER"] = per_value
+        self.val_logs["val_CER"] = cer_value
+        self.val_logs["val_Accuracy"] = acc
 
         self.val_logs = {k: v for k, v in self.val_logs.items()}
         for k, v in self.val_logs.items():
