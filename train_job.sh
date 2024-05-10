6cbb032c32f384ce47dd8d1f15f68eaf9e73ce50
Annesya Banerjee <annesyab@ranvier.ib.cluster>
2024-05-09 22:51:50 -0400

    changed paths for my directory

--- train_job.sh
+++ train_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=metamer
-#SBATCH --output=/om2/user/gelbanna/job_logs/job_%j.out
-#SBATCH --error=/om2/user/gelbanna/job_logs/job_%j.err
+#SBATCH --job-name=saganet_metamer
+#SBATCH --output=/om2/user/annesyab/SLP_Project_2024/voice-speech-metamers/job_logs/job_%j.out
+#SBATCH --error=/om2/user/annesyab/SLP_Project_2024/voice-speech-metamers/job_logs/job_%j.err
 #SBATCH --mem=25Gb
 #SBATCH --gres=gpu:a100:2
 #SBATCH --cpus-per-task=16
@@ -12,6 +12,6 @@ source /etc/profile.d/modules.sh
 module load openmind/miniconda/3.9.1
 module load openmind/git-lfs/2.13.2
 
-source activate /om2/user/gelbanna/miniconda3/envs/metamer310
+source activate /om2/user/annesyab/summer2023/anaconda/envs/metamer_310
 
 srun python train.py
